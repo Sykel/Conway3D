a299f8c207c336289cae814a01226f8a31ed4aea
Harvey Chan <harveychan92@gmail.com>
2016-07-27 19:04:40 -0700

    initial commit

---
+++ Assets/Cube.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Cube : MonoBehaviour
+{
+
+    // Use this for initialization
+    private MeshRenderer meshRenderer;
+
+    public List<GameObject> neighbors;
+
+    private int counter;
+    private int speed;
+
+    public void setNeighbours(List<GameObject> neighboursList)
+    {
+        neighbors = neighboursList;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = UnityEngine.Random.Range(0, 2) == 1;
+        counter = 0;
+        speed = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            speed--;
+        }
+
+        if (counter < speed)
+        {
+            counter++;
+            return;
+        }
+
+        counter = 0;
+        //if (Input.GetKeyUp(KeyCode.M))
+        //{
+            int aliveMinimum = 1;
+            int aliveMaximum = 2;
+            int deadMinimum = 4;
+            int deadMaximum = 4;
+
+            int numNeighbours = getNumNeighbours(neighbors);
+
+            if (meshRenderer.enabled == false)
+            {
+                if (deadMinimum <= numNeighbours && numNeighbours <= deadMaximum)
+                {
+                    meshRenderer.enabled = true;
+                }
+                else
+                {
+                    meshRenderer.enabled = false;
+                }
+            }
+            else
+            {
+                if (aliveMinimum <= numNeighbours && numNeighbours <= aliveMaximum)
+                {
+                    meshRenderer.enabled = true;
+                }
+                else
+                {
+                    meshRenderer.enabled = false;
+                }
+            }
+
+            //meshRenderer.enabled = Random.Range(0, 2) == 1;
+        //}
+    }
+
+    private int getNumNeighbours(List<GameObject> neighbors)
+    {
+        int numNeighbours = 0;
+        foreach (var neighbour in neighbors)
+        {
+            if (neighbour.GetComponent<Cube>().meshRenderer.enabled == true)
+            {
+                numNeighbours++;
+            }
+        }
+        return numNeighbours;
+    }
+
+    void Awake()
+    {
+// Debug.Log("Awake called.");
+    }
+
+
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GameManager : MonoBehaviour {
+    public int size;
+    public GameObject globalCube;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject[,,] cubeList = new GameObject[size, size, size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    GameObject thisCube = (GameObject)Instantiate(globalCube);
+                    thisCube.transform.position = new Vector3(i, j, k);
+
+                    cubeList[i, j, k] = thisCube;
+                    //thisCube.GetComponent<Neighbour>();
+                }
+            }
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                for (int k = 0; k < size; k++)
+                {
+                    // Get neighbours
+                    GetNeighbours(i, j, k, cubeList);
+
+                }
+            }
+        }
+    }
+
+    private static void GetNeighbours(int x, int y, int z, GameObject[,,] cubeList)
+    {
+        List<GameObject> neighbours = new List<GameObject>();
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                for (int k = -1; k <= 1; k++)
+                {
+
+                    if (i == 0 && j == 0 && k == 0)
+                    {
+                        continue;
+                    }
+                    AddNeighbour(x + i, y + j, z + k, cubeList, neighbours);
+                    //Debug.Log("Adding neighbour to " + x + " " + y + " " + z + " " + i + " " + j + " " + k);
+                }
+            }
+        }
+        cubeList[x, y, z].GetComponent<Cube>().setNeighbours(neighbours);
+                    
+
+    }
+
+    private static void AddNeighbour(int i, int j, int k, GameObject[,,] cubeList, List<GameObject> neighbours)
+    {
+
+        try { neighbours.Add(cubeList[i, j, k]);
+            //Debug.Log("I DID ADD A NEIGHBOUR");
+        }
+        catch { }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
